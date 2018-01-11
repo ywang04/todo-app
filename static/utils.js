a717896bfd8933c86485e27f1133232a839b76ed
Yang Wang <Yang.Wang04@gmail.com>
2018-01-11 21:19:53 +1100

    Rewrite ajax function

--- static/utils.js
+++ static/utils.js
@@ -2,7 +2,7 @@
  * @Author: ywang04
  * @Date:   2018-01-03T10:00:51+11:00
  * @Last modified by:   ywang04
- * @Last modified time: 2018-01-08T15:09:02+11:00
+ * @Last modified time: 2018-01-11T10:18:15+11:00
  */
 
 var log = console.log.bind(console)
@@ -17,5 +17,18 @@ var bindEvent = function(element, eventType, callback) {
 
 var ajax = function(request) {
   var r = new XMLHttpRequest()
-  r.open(request.method, request.)
+  r.open(request.method, request.url, true)
+  if (request.contentType !== undefined) {
+    r.setRequestHeader('Content-Type', request.contentType)
+  }
+  r.onreadystatechange = function() {
+    if (r.readyState === 4) {
+      request.responseCallback(r)
+    }
+  }
+  if (request.method === "GET") {
+    r.send()
+  } else {
+    r.send(request.data)
+  }
 }

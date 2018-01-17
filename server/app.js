284af861eebdf3a1239afca7a824739c06dbeac7
Yang Wang <Yang.Wang04@gmail.com>
2018-01-17 16:27:10 +1100

    Modify app.js under server folder
    1. Add view engine setup
    2. Require and use routes for index and todo

--- server/app.js
+++ server/app.js
@@ -2,72 +2,27 @@
  * @Author: ywang04
  * @Date:   2017-12-18T09:11:51+11:00
  * @Last modified by:   ywang04
- * @Last modified time: 2018-01-16T21:25:44+11:00
+ * @Last modified time: 2018-01-17T16:19:49+11:00
  */
 
 
 var express = require('express')
-var app = express()
-var todoRouter = express.Router()
+var path = require('path')
 var bodyParser = require('body-parser')
+var indexRouter = require('./routes/index')
+var todoRouter = require('./routes/todo')
 
+var app = express()
 
-app.use(express.static('static'))
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.html', ejs.__express)
+app.set('view engine', 'html')
+
 app.use(bodyParser.json())
+app.use('/', indexRouter)
+app.use('/api/todo/', todoRouter)
 
-var todo = require('./todo.js')
-
-var sendHtml = function(path, response) {
-  var fs = require('fs')
-  fs.readFile(path, 'utf-8', function(err, data) {
-    if (err) {
-      throw err
-    } else {
-      response.send(data)
-    }
-  })
-}
-
-app.get('/', function(request, response) {
-  var path = "index.html"
-  sendHtml(path, response)
+app.listen(3000, function() {
+  console.log("listening on port 3000");
 })
-
-app.post('/api/todo/add', function(request, response) {
-  var data = request.body
-  var r = todo.add(data)
-  response.send(r)
-})
-
-app.get('/api/todo/delete/:id', function(request, response) {
-  var id = parseInt(request.params.id)
-  var r = todo.delete(id)
-  response.send(r)
-})
-
-app.post('/api/todo/update/:id', function(request, response) {
-  var id = parseInt(request.params.id)
-  var data = request.body
-  var task = data.task
-  var r = todo.update(id, task)
-  response.send(r)
-})
-
-app.get('/api/todo/all', function(request, response) {
-  var data = todo.all()
-  response.send(data)
-})
-
-app.get('/api/todo/done/:id', function(request, response) {
-  var id = parseInt(request.params.id)
-  var r = todo.done(id)
-  response.send(r)
-})
-
-app.get('/api/todo/undo/:id', function(request, response) {
-  var id = parseInt(request.params.id)
-  var r = todo.undo(id)
-  response.send(r)
-})
-
-app.listen(3000)

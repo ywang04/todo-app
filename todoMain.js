b8de68661af84e5f41b5658e92d96214a69dd524
Yang Wang <Yang.Wang04@gmail.com>
2018-01-29 22:30:50 +1100

    Move todoMain.js to root folder

--- todoMain.js
+++ todoMain.js
@@ -2,8 +2,18 @@
  * @Author: ywang04
  * @Date:   2018-01-03T10:00:51+11:00
  * @Last modified by:   ywang04
- * @Last modified time: 2018-01-17T22:52:30+11:00
+ * @Last modified time: 2018-01-22T20:47:06+11:00
  */
+
+import Vue from 'vue'
+import App from '../app.vue'
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+new Vue({
+  render: (h) => h(App)
+}).$mount(root)
 
 var utils = require('../util/utils')
 var todo = require('../service/todo-service')

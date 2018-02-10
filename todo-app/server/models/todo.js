90019f35b5cb6f46a77b53c419b576deab2d6949
Yang Wang <Yang.Wang04@gmail.com>
2018-02-10 15:50:49 +1100

    Move todo.js from /server_back/models folder to destination folder

--- todo-app/server/models/todo.js
+++ todo-app/server/models/todo.js
@@ -6,9 +6,9 @@
  */
 
 var fs = require('fs')
-var dataFile = './server/db/todo.json'
+var dataFile = '../server/db/todo.json'
 
-var loadTodosFromFile = function() {
+var loadTodosFromFile = function () {
   var content = fs.readFileSync(dataFile, 'utf-8')
   var todos = JSON.parse(content)
   return todos
@@ -18,23 +18,23 @@ var t = {
   data: loadTodosFromFile()
 }
 
-t.writeTodosToFile = function() {
+t.writeTodosToFile = function () {
   var data = JSON.stringify(this.data)
-  fs.writeFile(dataFile, data, function(err) {
+  fs.writeFile(dataFile, data, function (err) {
     if (err) {
       throw err
     } else {
-      console.log("todo has been saved.")
+      console.log('todo has been saved.')
     }
   })
 }
 
-t.all = function() {
+t.all = function () {
   return this.data
 }
 
 
-t.add = function(todo) {
+t.add = function (todo) {
   var length = this.data.length
   if (length) {
     todo.id = this.data[this.data.length - 1].id + 1
@@ -46,7 +46,7 @@ t.add = function(todo) {
   return todo
 }
 
-t.delete = function(id) {
+t.delete = function (id) {
   var todos = this.data
   for (var i = 0; i < todos.length; i++) {
     if (id === todos[i].id) {
@@ -58,7 +58,7 @@ t.delete = function(id) {
   return `todo ${id} doesn't exist.`
 }
 
-t.update = function(id, task) {
+t.update = function (id, task) {
   var todos = this.data
   for (var i = 0; i < todos.length; i++) {
     if (id === todos[i].id) {
@@ -72,7 +72,7 @@ t.update = function(id, task) {
   return `todo ${id} does not exist.`
 }
 
-t.done = function(id) {
+t.done = function (id) {
   var todos = this.data
   for (var i = 0; i < todos.length; i++) {
     if (id === todos[i].id) {
@@ -84,7 +84,7 @@ t.done = function(id) {
   }
 }
 
-t.undo = function(id) {
+t.undo = function (id) {
   var todos = this.data
   for (var i = 0; i < todos.length; i++) {
     if (id === todos[i].id) {
